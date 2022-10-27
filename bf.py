ccc4e038c578863bb09189be10e5161a82cb8c9b
birwas <birwashiwani@hotmail.com>
2022-10-27 20:38:04 +0100

    Getting HW working
    Fixed the loop interpreting and some typos with increment/decre

--- bf.py
+++ bf.py
@@ -30,6 +30,9 @@ if(bracketCheck!=-1):
     if len(matching):
         print("missing closed bracket from pos " + str(matching[0]))
         quit()
+    
+
+loopCycles=0
 while(pC<len(program)):
     instr = program[pC]
     if(instr==">"):
@@ -37,9 +40,9 @@ while(pC<len(program)):
         if(dPointer==len(data)):
             data.append(0)
     elif(instr=="<"):
-        dPointer-+1
+        dPointer-=1
     elif(instr=="+"):
-        data[dPointer]=+1
+        data[dPointer]+=1
         if(data[dPointer]>255):
             data[dPointer]=0
         
@@ -60,12 +63,16 @@ while(pC<len(program)):
     elif(instr=="["):
         if(data[dPointer]==0):
             pc=brackets[pC]
+            continue
 
     elif(instr=="]"):
         if(data[dPointer]!=0):
-            pc=brackets[pC]
+            pC=brackets[pC]
+            loopCycles+=1
+            continue
+
     pC+=1
 
     
     
-#print(data)
+
